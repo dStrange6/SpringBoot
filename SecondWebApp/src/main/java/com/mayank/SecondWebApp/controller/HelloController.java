0e141bfd6d6de2fe6ebe11a50b1da9b22ac1ef89
Mayank Pathak <pmayank@Mayanks-Laptop.local>
2025-02-16 01:33:54 +0530

    mayank

--- SecondWebApp/src/main/java/com/mayank/SecondWebApp/controller/HelloController.java
+++ SecondWebApp/src/main/java/com/mayank/SecondWebApp/controller/HelloController.java
@@ -1,10 +1,13 @@
 package com.mayank.SecondWebApp.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -35,5 +38,17 @@ public class HelloController
         Map<String, Object> model = new HashMap<>();  // we have created the model
         model.put("message", "Hello from new page!");
         return model;
+    }
+//    @GetMapping("/user")
+//    public void userPage(HttpServletResponse response) throws IOException {
+//        PrintWriter writer = response.getWriter();
+//        writer.println("<h1>Welcome to user Page.....</h1>");
+//    }
+    @GetMapping("/user")
+    public String userPage(Map<String, Object> model)
+    {
+        User user = new User(1,"Mayank Pathak",25);
+        model.put("user", user);   //adding user object
+        return "user";
     }
 }
